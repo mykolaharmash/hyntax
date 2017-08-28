045616ec80cf26b2e7f7260507e939716302c438
Nikolay Garmash <garmash.nikolay@gmail.com>
2017-08-28 13:57:08 +0200

    Parse
    — splits parse.js into set of
      modular parsers
    — adds parsing into generate.js

--- generate.js
+++ generate.js
@@ -2,9 +2,13 @@ let fs = require('fs')
 let path = require('path')
 
 let tokenize = require('./lib/tokenize')
+let parse = require('./lib/parse')
+let prettyJSON = require('./lib/helpers').prettyJSON
 
 let template = fs.readFileSync(path.resolve(__dirname, './template.html'), 'utf8')
 
 let tokens = tokenize(template)
+let ast = parse(tokens)
 
-console.log(tokens)
+fs.writeFileSync('./tmp/tokens.json', prettyJSON(tokens))
+fs.writeFileSync('./tmp/ast.json', prettyJSON(ast))
