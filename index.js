532c20f63c0d2f72a2f6da8a6b8624b64f8482d1
Nikolay Garmash <garmash.nikolay@gmail.com>
2017-11-19 19:59:21 +0100

    Index File
    — cleans up package structure
    — creates index.js with all available
      modules

---
+++ index.js
@@ -0,0 +1,11 @@
+const tokenize = require('./lib/tokenize')
+const parse = require('./lib/parse')
+const TokenizeStream = require('./lib/tokenize-stream')
+const ParseStream = require('./lib/parse-stream')
+
+module.exports = {
+  tokenize,
+  parse,
+  TokenizeStream,
+  ParseStream
+}
