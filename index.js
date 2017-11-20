734e3dd4085e9eb33ed80804db450f98f93f8376
Nikolay Garmash <garmash.nikolay@gmail.com>
2017-11-20 08:38:43 +0100

    Browser Bundling
    — fixes named exports for
      rollup

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const parse = require('./lib/parse')
 const TokenizeStream = require('./lib/tokenize-stream')
 const ParseStream = require('./lib/parse-stream')
 
-module.exports = {
-  tokenize,
-  parse,
-  TokenizeStream,
-  ParseStream
-}
+// Needs to be separate exports
+// in order to be properly bundled
+// and recognised by Rollup as named
+// exports
+module.exports.tokenize = tokenize
+module.exports.parse = parse
+module.exports.TokenizeStream = TokenizeStream
+module.exports.ParseStream = ParseStream
